63bb715765d278a592561acad9d5b7731de7efa0
Punyawat18 <Punyawat_pun28@hotmail.com>
2025-01-13 12:04:46 +0700

    rewrote some logic

--- circle.py
+++ circle.py
@@ -4,44 +4,46 @@ import random
 screen = pygame.display.set_mode((500, 500))
 pygame.time.Clock().tick(10)
 
+def move(circle):
+
+    if circle.location[0] == 0:
+        circle.status0 = "up"
+    elif circle.location[0] == 500:
+        circle.status0 = "down"
+    if circle.location[1] == 0:
+        circle.status1 = "up"
+    elif circle.location[1] == 500:
+        circle.status1 = "down"
+    if circle.status0 == "up":
+        circle.location[0] += 1
+    else:
+        circle.location[0] -= 1
+    if circle.status1 == "up":
+        circle.location[1] += 1
+    else:
+        circle.location[1] -= 1
+
+    circle.color[0] = random.randint(0, 255)
+    circle.color[1] = random.randint(0, 255)
+    circle.color[2] = random.randint(0, 255)
+
+
 class Circle:
-    def __init__(self, radius, location=[500,500]):
+    def __init__(self, radius, location=[500,500], color=[255, 255, 255], status0="up", status1="up"):
         self.radius = radius
         self.location = location
+        self.color = color
+        self.status0 = status0
+        self.status1 = status1
 
-    def draw(self, color, location):
-        pygame.draw.circle(screen, color, location, self.radius)
-        pygame.display.flip()
-
-    def bounce(self):
-        location = self.location
-        color = [255, 255, 255]
-        status0 = "up"
-        status1 = "up"
-        while True:
-            self.draw(color, location)
-            screen.fill((0, 0, 0))
-            if location[0] == 500:
-                status0 = "down"
-            if location[1] == 500:
-                status1 = "down"
-            if location[0] == 0:    
-                status0 = "up"
-            if location[1] == 0:
-                status1 = "up"
-            if status0 == "up":
-                location[0] += 1
-            else:
-                location[0] -= 1
-            if status1 == "up":
-                location[1] += 1
-            else:
-                location[1] -= 1
-            color[0] = random.randint(0, 255)
-            color[1] = random.randint(0, 255)
-            color[2] = random.randint(0, 255)
+    def draw(self):
+        pygame.draw.circle(screen, self.color, self.location, self.radius)
 
 
+mycircle1 = Circle(50, [0, 500])
+mycircle2 = Circle(50, [500, 0])
+mycircle3 = Circle(50, [500, 500])
+mycircle4 = Circle(50, [0, 0])
 
 while True:
     for event in pygame.event.get():
@@ -49,7 +51,14 @@ while True:
             pygame.quit()
             quit()
 
-    mycircle = Circle(50, [0, 500])
-    mycircle.bounce()
-
+    mycircle1.draw()
+    mycircle2.draw()
+    mycircle3.draw()
+    mycircle4.draw()
+    move(mycircle1)
+    move(mycircle2)
+    move(mycircle3)
+    move(mycircle4)
+    pygame.display.flip()
+    screen.fill((0, 0, 0))
 
